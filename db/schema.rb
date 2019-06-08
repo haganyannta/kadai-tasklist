d3891d416dac0465edc29d615964077a7626c01b
EC2 Default User <ec2-user@ip-172-31-89-235.ec2.internal>
2019-06-08 11:36:28 +0000

    reset

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_01_100131) do
+ActiveRecord::Schema.define(version: 2019_06_08_112723) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
