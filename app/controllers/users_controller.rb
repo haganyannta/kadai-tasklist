3f463554b8e4961029f419a2dce6d94fcee8d2d7
EC2 Default User <ec2-user@ip-172-31-89-235.ec2.internal>
2019-06-10 10:20:32 +0000

    login page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :require_user_logged_in, only: [:index, :show]
+  
   def index
     @users = User.order(id: :desc)
   end
@@ -18,7 +18,7 @@ class UsersController < ApplicationController
     
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to @user
+      redirect_to tasks_url
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
